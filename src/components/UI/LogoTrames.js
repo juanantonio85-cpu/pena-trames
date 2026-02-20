e779357ee812b0f0eeb4d6bfe482a47c836aae79
Juanan <juanantonio85@gmail.com>
2026-02-20 12:10:24 +0100

    Fix: logo import for Vercel

--- src/components/UI/LogoTrames.js
+++ src/components/UI/LogoTrames.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./LogoTrames.css";
-import logo from "../../logo.png";
 
 export default function LogoTrames({ size = 140 }) {
   return (
     <div className="trames-logo-wrapper">
       <img
-        src={logo}
+        src="/logo.png"
         alt="TRAMES FC"
         className="trames-logo"
         style={{ width: size, height: size }}
